f9f39654aa29748da916d9e9daf2a6b67bad309c
RuzaikinDY <Devilwind@mail.ru>
2022-05-25 22:26:44 +0300

    Reformat code

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,6 +17,7 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
     @Test
     void findMaxincenter() {
         StatisticsService service = new StatisticsService();
